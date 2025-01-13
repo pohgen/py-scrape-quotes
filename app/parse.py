03e54da5c6ba2c255ab56f07949f5eed32db8ada
Alex <alex.gal.trot@gmail.com>
2025-01-13 21:41:07 +0100

    fix flake8 9

--- app/parse.py
+++ app/parse.py
@@ -30,13 +30,13 @@ def extract_bio(author: str) -> None:
 
     url = urljoin(
         URL,
-        f"author/{author
-        .replace('. ', '-')
-        .replace(' ', '-')
-        .replace('.', '-')
-        .replace('\'', '')
-        .replace('é', 'e')
-        .rstrip('-')}/")
+        f'author/{author.replace(". ", "-")
+        .replace(" ", "-")
+        .replace(".", "-")
+        .replace("\'", "")
+        .replace("é", "e")
+        .rstrip('-')}/'
+    )
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
 
